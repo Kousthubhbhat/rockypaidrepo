c30724823e9058505a24f73235b08e5c19557771
Kousthubhbhat <93434418+Kousthubhbhat@users.noreply.github.com>
2022-03-15 19:04:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,19 +28,19 @@ bot = Client('Droplink bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}⚡**\n"
-        "**🤖I Am (Not At All 😏) Simple Bot \n🔀 I can Convert Your Links To Du-link \n📁 Send ME Image or Text_Message \n🔃 Convert All Links To Your Links \n👨🏻‍💻 I Was Developed  @Half_Intelegent_2 \n©️ Powered By @DuLinkUpdate**")
+        "**🤖I Am (Not At All 😏) Simple Bot \n🔀 I can Convert Your Links To Du-link \n📁 Send ME Image or Text_Message \n🔃 Convert All Links To Your Links \n👨🏻‍💻 I Was Developed  @Half_Intelligent_2 \n©️ Powered By @DuLinkUpdate**")
 
 @bot.on_message(filters.command('help') & filters.private)
 async def start(bot, message):
     await message.reply(
         f"**Hello, {message.chat.first_name}!**\n\n"
-        "**If you send post which had Links, texts & images... Than I'll convert & replace all links with your links \nMessage me  @Half_Intelegent_2 For more help-**")
+        "**If you send post which had Links, texts & images... Than I'll convert & replace all links with your links \nMessage me @Half_Intelligent_2 For more help-**")
 
 @bot.on_message(filters.command('support') & filters.private)
 async def start(bot, message):
     await message.reply(
         f"**Hey, {message.chat.first_name}!**\n\n"
-        "**please contact me @Half_Intelegent_2 **")
+        "**please contact me @Half_Intelligent_2**")
     
 @bot.on_message(filters.text & filters.private)
 async def pdisk_uploader(bot, message):
