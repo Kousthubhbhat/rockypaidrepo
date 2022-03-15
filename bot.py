53086462a293013070d05e0924e3d45fedaada35
Kousthubhbhat <93434418+Kousthubhbhat@users.noreply.github.com>
2022-03-15 17:24:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ async def pdisk_uploader(bot, message):
     
 async def pdisk_up(link):
     if ('mega' in link or 'google' in link or 'mdisk' in link or 'entertainvideo' in link or 'dood' in link or 'bit' in link ):
-        url = 'https://droplink.co/api'
+        url = 'https://du-link.in/api'
         params = {'api': API_KEY, 'url': link}
     
         async with aiohttp.ClientSession() as session:
